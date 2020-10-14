7de13b75d4705ae73ffab868453581d4749d3894
Florian Pester <florian.pester@cyberus-technology.de>
2020-10-14 20:47:07 +0200

    sensors: add dht22

---
+++ sensors/dht22.py
@@ -0,0 +1,33 @@
+import datetime
+import time
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.AM2302
+pin = 23
+
+class Sensor:
+    def id(self):
+        return "DHT22"
+
+    def get_value(self):
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        return { "Time" : datetime.datetime.now().isoformat(),
+                "DHT22" : { "Temperature" : temperature, "Humidity" : humidity },
+                 "TempUnit" : "C"
+               }
+
+    def templates(self, device_id, room):
+        myid = self.id()
+        return [{
+            "name": "{} {} Temperature".format(room, myid),
+            "uniq_id": "{}_{}_Temperature".format(device_id, myid),
+            "unit_of_meas": "°C",
+            "val_tpl": "{{value_json['DHT22'].Temperature}}",
+            "dev_cla": "temperature"
+        },{
+            "name": "{} {} Humidity".format(room, myid),
+            "uniq_id": "{}_{}_Humidity".format(device_id, myid),
+            "unit_of_meas": "%",
+            "val_tpl": "{{value_json['DHT22'].Humidity}}",
+            "dev_cla": "humidity"
+        }]
